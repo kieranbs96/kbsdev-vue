e29ef5f3e943704a1150e0254c591afdd05c7f24
kieranbs96 <kieranbs96@gmail.com>
2018-03-31 15:01:11 +0100

    added Pug & VueJS

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,98 +1,64 @@
-<template>
-                <div class="about">
-                    <div class="container">
-                        <h2 class="about__heading">👋 My name is Kieran</h2>
-                        <div class="about__text">
-                            I'm an enthusiastic Junior Web Developer based in London, I am very keen to learn and master the most modern and advanced
-                            technologies. In my leisure time participate in and enjoy swimming, cycling and playing as well
-                            as watching football, another hobby includes reading articles and blogs on Medium to keep me
-                            up to date in the Web Development industry my favourite publishers include: Freecodecamp, Hackernoon
-                            and Codeburst. In the list below you can find my relevant Web Development skills.
-                        </div>
-                        <div class="about__skills">
-                            <div class="about__skill">
-                                <span class="about__skill-icon about__skill-icon--html">
-                                    <i class="fab fa-html5"></i>
-                                </span>
-                                <p class="about__skill-text">
-                                    HTML5
-                                </p>
-                            </div>
-                            <div class="about__skill">
-                                <span class="about__skill-icon about__skill-icon--css">
-                                    <i class="fab fa-css3-alt"></i>
-                                </span>
-                                <p class="about__skill-text">
-                                    CSS3
-                                </p>
-                            </div>
-                            <div class="about__skill">
-                                <span class="about__skill-icon about__skill-icon--js">
-                                    <i class="fab fa-js"></i>
-                                </span>
-                                <p class="about__skill-text">
-                                    Javascript
-                                </p>
-                            </div>
-                            <div class="about__skill">
-                                <span class="about__skill-icon about__skill-icon--css">
-                                    <i class="fab fa-sass"></i>
-                                </span>
-                                <p class="about__skill-text">
-                                    SASS
-                                </p>
-                            </div>
-                            <div class="about__skill">
-                                <span class="about__skill-icon about__skill-icon--git">
-                                    <i class="fab fa-github"></i>
-                                </span>
-                                <p class="about__skill-text">
-                                    Github
-                                </p>
-                            </div>
-                            <div class="about__skill">
-                                <span class="about__skill-icon about__skill-icon--npm">
-                                    <i class="fab fa-npm"></i>
-                                </span>
-                                <p class="about__skill-text">
-                                    NPM
-                                </p>
-                            </div>
-                            <div class="about__skill">
-                                <span class="about__skill-icon about__skill-icon--gulp">
-                                    <i class="fab fa-gulp"></i>
-                                </span>
-                                <p class="about__skill-text">
-                                    Gulp
-                                </p>
-                            </div>
-                            <div class="about__skill">
-                                <span class="about__skill-icon about__skill-icon--node">
-                                    <i class="fab fa-node"></i>
-                                </span>
-                                <p class="about__skill-text">
-                                    Node.js
-                                </p>
-                            </div>
-                            <div class="about__skill">
-                                <span class="about__skill-icon about__skill-icon--react">
-                                    <i class="fab fa-react"></i>
-                                </span>
-                                <p class="about__skill-text">
-                                    React
-                                </p>
-                            </div>
-                            <div class="about__skill">
-                                <span class="about__skill-icon about__skill-icon--react">
-                                    <i class="fal fa-compress"></i>
-                                </span>
-                                <p class="about__skill-text">
-                                    RWD
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+<template lang="pug">
+mixin svg-icon(name, size)
+    - var size = size || 24;
+
+div.about
+  div.container
+    h2.about__heading 👋 My name is Kieran
+    div.about__text
+      p I'm an enthusiastic Junior Web Developer based in London, I am very keen to learn and master the most modern and advanced technologies. In my leisure time participate in and enjoy swimming, cycling and playing as well as watching football, another hobby includes reading articles and blogs on Medium to keep me up to date in the Web Development industry my favourite publishers include: Freecodecamp, Hackernoon and Codeburst. In the list below you can find my relevant Web Development skills.
+
+    div.about__skills
+      div.about__skill
+        span.about__skill-icon.about__skill-icon--html
+          i.fab.fa-html5
+        p.about__skill-text HTML5
+      div.about__skill
+        span.about__skill-icon.about__skill-icon--css
+          i.fab.fa-css3-alt
+        p.about__skill-text CSS3
+      .about__skill
+        span.about__skill-icon.about__skill-icon--js
+          i.fab.fa-js
+        p.about__skill-text Javascript
+      .about__skill
+        span.about__skill-icon.about__skill-icon--css
+          i.fab.fa-sass
+        p.about__skill-text SASS
+      .about__skill
+        span.about__skill-icon.about__skill-icon--git
+          i.fab.fa-github
+        p.about__skill-text Github
+      .about__skill
+        span.about__skill-icon.about__skill-icon--npm
+          i.fab.fa-npm
+        p.about__skill-text NPM
+      .about__skill
+        span.about__skill-icon.about__skill-icon--gulp
+          i.fab.fa-gulp
+        p.about__skill-text Gulp
+      .about__skill
+        span.about__skill-icon.about__skill-icon--node
+          i.fab.fa-node
+        p.about__skill-text Node.js
+      .about__skill
+        span.about__skill-icon.about__skill-icon--react
+          i.fab.fa-react
+        p.about__skill-text React
+      .about__skill
+        span.about__skill-icon.about__skill-icon--react
+          i.fal.fa-compress
+        p.about__skill-text RWD
+      .about__skill
+        span.about__skill-icon.about__skill-icon--vue
+          i.fab.fa-vuejs
+        p.about__skill-text VueJS
+      .about__skill
+        span.about__skill-icon.about__skill-icon--pug
+          img.about__skill-image.about__skill-image-w(src="../assets/svg/pug-w.png" alt="Pug")
+          img.about__skill-image.about__skill-image-c(src="../assets/svg/pug-c.png" alt="Pug")
+        p.about__skill-text Pug
+
 </template>
 
 <script>
